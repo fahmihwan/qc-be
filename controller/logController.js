97720382271fe5b3ce47de532e805dd10cafe3d6
fahmihwan <50146950+fahmihwan@users.noreply.github.com>
2025-03-17 15:35:53 +0700

    add log error

--- controller/logController.js
+++ controller/logController.js
@@ -2,7 +2,7 @@ const logger = require("../config/logging")
 
 const logFe = async (req, res) => {
 
-    // logger.error(req.body.error)
+    logger.error(req.body?.error)
     res.status(200).send({
         data: req.body?.error
     })
