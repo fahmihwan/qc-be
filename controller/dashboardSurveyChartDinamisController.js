e6193eee9d0bfde08432534f55f54998ff7ebec0
fahmihwan <50146950+fahmihwan@users.noreply.github.com>
2025-03-19 11:36:33 +0700

    add query bar-keberlanjutanproduksipangan

--- controller/dashboardSurveyChartDinamisController.js
+++ controller/dashboardSurveyChartDinamisController.js
@@ -18,9 +18,6 @@ const chartDashboardSurveyDinamis = async (req, res) => {
         console.log(req.body)
         let getBody = req.body.body
 
-        // let resultPie;
-        // let resultBar;
-        // let resultLine = '';
 
         let results = [];
         for (let i = 0; i < getBody.length; i++) {
@@ -110,38 +107,99 @@ const chartDashboardSurveyDinamis = async (req, res) => {
             }
 
             if (getBody[i].type == 'bar-realisasidistribusipangan') {
-                let caseWhen = body_name_input + '-Comment'
                 let name_inputLike = `%${body_name_input}%`
-                let params = [body_topikId, body_title, name_inputLike, caseWhen]
-
+                let titleLike = `%${body_title}%`
+                let params = [body_topikId, titleLike, name_inputLike,]
                 let whereClause = ''
+
+                let resultBar = ""
                 if (provinsi_id != undefined) {
-                    whereClause += `and r.provinsi_id = $5`;
+                    whereClause += `and r.provinsi_id = $4`;
                     params.push(Number(provinsi_id))
+
+
+                    // per kota FIX <==========  
+                    resultBar = await prisma.$queryRawUnsafe(`select 
+                                        x.nama_kabupaten_kota, count(x.bermasalah) as bermasalah, count(x.lancar) as lancar
+                                        from (
+                                        with last_insert_from_kabkota as (
+                                            SELECT 
+                                                r.id, r.provinsi_id , r.kabkota_id, r.kecamatan_id, r.created_at,
+                                                ROW_NUMBER() OVER (PARTITION BY r.kecamatan_id ORDER BY r.created_at DESC) AS last_data
+                                            FROM  responden r WHERE  r.topik_id = $1 ${whereClause}
+                                        ) 
+                                        select 
+                                            k.nama_kabupaten_kota,
+                                            case WHEN dr.value = 'Tidak' THEN 'Bermasalah' end as bermasalah,
+                                            case WHEN dr.value = 'Ya' THEN 'Lancar' end as lancar,
+                                            dr.title 
+                                        from last_insert_from_kabkota lst
+                                            inner join detail_responden dr on dr.responden_id = lst.id 
+                                        --		inner join provinsi pr on  pr.provinsi_id = lst.provinsi_id
+                                            inner join kabupaten_kota k on k.kabkota_id = lst.kabkota_id
+                                            where last_data = 1 
+                                                and dr.title ilike $2
+                                                and dr.name_input ilike $3
+                                        ) as x
+                                        group by x.nama_kabupaten_kota
+                                        order by bermasalah desc limit 5`, ...params)
+
+                } else if (provinsi_id == undefined) {
+
+                    // per provinsi FIX <========
+                    resultBar = await prisma.$queryRawUnsafe(`
+                                        select 
+                                            x.nama_provinsi, count(x.bermasalah) as bermasalah, count(x.lancar) as lancar
+                                        from (
+                                            with last_insert_from_kabkota as (
+                                                SELECT 
+                                                    r.id, r.provinsi_id, r.kabkota_id, r.created_at,
+                                                    ROW_NUMBER() OVER (PARTITION BY r.kabkota_id ORDER BY r.created_at DESC) AS last_data
+                                                FROM 
+                                                    responden r
+                                                WHERE 
+                                                    r.topik_id = $1
+                                            ) 
+                                            select 
+                                                pr.nama_provinsi,
+                                                case 
+                                                    WHEN dr.value = 'Tidak' THEN 'Bermasalah'
+                                                end as bermasalah,
+                                                case 
+                                                    WHEN dr.value = 'Ya' THEN 'Lancar'
+                                                end as lancar,
+                                                dr.title 
+                                            from last_insert_from_kabkota lst
+                                                inner join detail_responden dr on dr.responden_id = lst.id 
+                                                inner join provinsi pr on  pr.provinsi_id = lst.provinsi_id
+                                                where last_data = 1 
+                                                    and dr.title ilike $2
+                                                    and dr.name_input ilike $3
+                                        ) as x
+                                        group by x.nama_provinsi
+                                        order by bermasalah desc  limit 5`, ...params)
+
+
                 }
 
-                let resultBar = await prisma.$queryRawUnsafe(`select year, value, count(value) from (
-                      select
-                            case 
-                                WHEN dr.value = 'Tidak' THEN 'Bermasalah'
-                                when dr.value = 'Ya' THEN 'Lancar'
-                            end as value,
-                            EXTRACT(YEAR FROM r.created_at) as year
-                        from responden r
-                            inner join topik tp on tp.id = r.topik_id
-                            inner join provinsi p on p.provinsi_id  = r.provinsi_id 
-                            inner join kabupaten_kota kk on kk.kabkota_id = r.kabkota_id 
-                            inner join detail_responden dr on r.id  = dr.responden_id 
-                        where r.topik_id = $1
-                            and dr.title = $2
-                            and dr.name_input ilike $3
-                            ${whereClause}
-                ) as x 
-                group by value, year
-                order by year desc`, ...params);
 
-                resultItem.data = fnMapBarChart(resultBar);
+
+
+
+                function replacer(key, value) {
+                    if (typeof value === 'bigint') {
+                        return value.toString();  // Convert BigInt to a string
+                    }
+                    return value;
+                }
+
+                resultItem.data = JSON.parse(JSON.stringify({
+                    data: resultBar,
+                }, replacer));
             }
+
+
+
 
             if (getBody[i].type == 'bar-tahun') {
                 let caseWhen = body_name_input + '-Comment'
