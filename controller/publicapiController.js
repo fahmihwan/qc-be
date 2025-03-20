93a371648fa3fb7a2ec44582c8b8b2cbe6589219
Margaretha Gok Asi Naibaho <margarethanaibaho2@gmail.com>
2025-03-20 14:07:48 +0700

    fix: add category "SEMUA" so the user can get all details for all categories in one hit

--- controller/publicapiController.js
+++ controller/publicapiController.js
@@ -92,30 +92,57 @@ const getBencana = async (req, res) => {
 const getByEachBencana = async (req, res) => {
     let { startDate, endDate, category, id } = req.query;
 
+    const listDropDownBencana = [
+        "BANJIR",
+        "TANAH LONGSOR",
+        "GEMPABUMI",
+        "ERUPSI GUNUNG API",
+        "CUACA EKSTREM",
+        "GELOMBANG PASANG DAN ABRASI",
+        "KEKERINGAN",
+        "KEBAKARAN HUTAN DAN LAHAN",
+        "TSUNAMI",
+        "GEMPA BUMI DAN TSUNAMI"
+    ]
+
    console.log("ini category", category)
     let prov = '';
     if (id !== undefined) {
         prov = `&id_prov=${id}`;
     }
 
-    let cat = '';
-    if(category !== undefined){
-        cat = `&kejadian='${category}'`;
-    }
-
-    let url = `https://gis.bnpb.go.id/databencana/kabupaten?start='${startDate}'&end='${endDate}'${prov}${cat}`
-
     try {
-        // Membuat permintaan HTTP dengan axios menggunakan async/await
-        const response = await axios.get(url);
+        if(category == "SEMUA") {
+            const requests = listDropDownBencana.map(async(bencana) => {
+                let url = `https://gis.bnpb.go.id/databencana/kabupaten?start='${startDate}'&end='${endDate}'${prov}&kejadian='${bencana}'`
+                const response = await axios.get(url)
+                return response.data.results
+            })
 
-        // Menampilkan data yang diterima dari response
-        console.log(response.data);
+            const results = await Promise.all(requests)
 
-        // Mengirimkan response ke client
-        res.status(200).send({
-            data: response.data // Mengirimkan data dari API yang di-fetch
-        });
+            const mergeData = results.flat()
+
+            console.log("masuk semua")
+
+            res.status(200).send({
+                data: {
+                    results: mergeData
+                }
+            })
+        } else {
+            let cat = '';
+            if(category !== undefined){
+                cat = `&kejadian='${category}'`;
+            }
+            
+            let url = `https://gis.bnpb.go.id/databencana/kabupaten?start='${startDate}'&end='${endDate}'${prov}${cat}`
+            const response = await axios.get(url)
+            
+            res.status(200).send({
+                data: response.data // Mengirimkan data dari API yang di-fetch
+            });
+        }
     } catch (error) {
         // Menangani error jika ada
         console.error('Error fetching data:', error);
@@ -124,7 +151,6 @@ const getByEachBencana = async (req, res) => {
         });
     }
 };
-
 
 
 module.exports = {getSummary, getBencana, getByEachBencana}
